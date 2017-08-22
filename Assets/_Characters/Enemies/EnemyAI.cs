a220a434b70a33002ae8e4775c41a9c033e2b2e8
Mazen Morgan <morgan.mazen42@gmail.com>
2017-08-22 02:02:14 +0200

    Setuping AI Behaviour Tree -> making the player chase us

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -6,26 +6,62 @@ using RPG.Core;
 namespace RPG.Characters
 {
     [RequireComponent(typeof(WeaponSystem))]
+    [RequireComponent(typeof(Character))]
     public class EnemyAI : MonoBehaviour//no Idamageable because we are going fron interface to component
     {
 
         [SerializeField] float chaseRadious = 6f;
-
-        bool isAttacking = false; //TODO more rich state (idle, attacking, chating , patrol,...etc)
+        enum State { idle, attacking, patrolling, chasing };
+        State state = State.idle;
+        float distanceToPlayer;
         float currentWeaponRange = 3f;
-
         PlayerMovement player = null;
+        Character character;
 
         private void Start()
         {
+            character = GetComponent<Character>();
             player = FindObjectOfType<PlayerMovement>();
         }
 
         private void Update()
         {
-            var distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             WeaponSystem weaponSystem = GetComponent<WeaponSystem>();
             currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
+
+            if(distanceToPlayer > chaseRadious && state != State.patrolling)
+            {
+                //stop what we'are doing
+                StopAllCoroutines();
+                //start patrolling
+                state = State.patrolling;
+
+            }
+            if (distanceToPlayer <=  chaseRadious && state != State.chasing)
+            {
+                //stop what we'are doing
+                StopAllCoroutines();
+                //start chasing
+                StartCoroutine(ChasePlayer());
+            }
+            if(distanceToPlayer <= currentWeaponRange && state != State.attacking)
+            {
+                //stop what we're doing
+                StopAllCoroutines();
+                //start attacking
+                state = State.attacking;
+            }
+        }
+
+        IEnumerator ChasePlayer()
+        {
+            state = State.chasing;
+            while(distanceToPlayer >= currentWeaponRange)
+            {
+                character.SetDestination(player.transform.position);
+                yield return new WaitForEndOfFrame();
+            }
         }
 
         //TODO We will simplify this Method in another way, we Can use this one if we didn't like the otherway.
