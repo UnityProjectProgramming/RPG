6cd39f46ed7b163dcfc5c37bfd8ac384453e676e
Mazen Morgan <morgan.mazen42@gmail.com>
2017-09-08 21:19:57 +0200

    Fixing A bug with windmill rotation

--- Assets/_Core/SpinMe.cs
+++ Assets/_Core/SpinMe.cs
@@ -7,23 +7,11 @@ namespace RPG.Core
     public class SpinMe : MonoBehaviour
     {
 
-        [SerializeField]
-        float xRotationsPerMinute = 1f;
-        [SerializeField]
-        float yRotationsPerMinute = 1f;
-        [SerializeField]
-        float zRotationsPerMinute = 1f;
+        [SerializeField] float zRotationsPerMinute = 1f;
 
-        void Update()
+        void FixedUpdate()
         {
-            float xDegreesPerFrame = (Time.deltaTime / 60) * xRotationsPerMinute * 360;
-            transform.RotateAround(transform.position, transform.right, xDegreesPerFrame);
-
-            float yDegreesPerFrame = (Time.deltaTime / 60) * yRotationsPerMinute * 360;
-            transform.RotateAround(transform.position, transform.up, yDegreesPerFrame);
-
             float zDegreesPerFrame = (Time.deltaTime / 60) * zRotationsPerMinute * 360;
-
             transform.RotateAround(transform.position, transform.forward, zDegreesPerFrame);
         }
     }
