1835fbff760007064f066fcfbc06a2428a3a5305
MazenMorgan <morgan.mazen42@gmail.com>
2019-01-24 00:45:35 +0200

    Dialogue Base

---
+++ RPG Game/Assets/_Core/Dialogue/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    public string NPCName;
+
+    [TextArea(3, 10)]
+    public string[] sentences;
+   
+}
