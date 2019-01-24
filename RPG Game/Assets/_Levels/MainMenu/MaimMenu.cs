117abe25e4f5d9d52fdd6f9545d9686fbe95d8c8
Heba\user <hh440555@gmail.com>
2019-01-24 15:18:38 +0200

    FINISHED SIMPLE START MAIN MENU

---
+++ RPG Game/Assets/_Levels/MainMenu/MaimMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MaimMenu : MonoBehaviour {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
