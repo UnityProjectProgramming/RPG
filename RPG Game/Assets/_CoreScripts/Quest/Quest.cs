dcb0be042bd82f2e25869c3d9d6f4fb7ffc3df38
MazenMorgan <morgan.mazen42@gmail.com>
2019-01-25 18:59:02 +0200

    Creating Base Quest Class

---
+++ RPG Game/Assets/_CoreScripts/Quest/Quest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Quest : MonoBehaviour
+{
+    public List<Goal> goals = new List<Goal>();
+    public string QuestName;
+    public string Questdescription;
+    public int experienceAmountReward;
+    // We can add here and item as a reward such as sword, potion, key .... etc.
+    public bool completed;
+
+    public void CheckGoals()
+    {
+        completed = goals.All(g => g.completed);
+        if(completed)
+        {
+            GiveReward();
+        }
+    }
+
+    private void GiveReward()
+    {
+        // Handle Giving rewards here and increasing Player EXP.
+    }
+
+}
