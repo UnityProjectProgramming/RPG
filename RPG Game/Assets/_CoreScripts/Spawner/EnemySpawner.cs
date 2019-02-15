7c5ff19906f2a391b1ab3c4b4d4185880fd15320
MazenMorgan <morgan.mazen42@gmail.com>
2019-02-15 03:20:13 +0200

    Creating ObjectPooler and EnemySpawner to spawn enemies

---
+++ RPG Game/Assets/_CoreScripts/Spawner/EnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+
+public class EnemySpawner : MonoBehaviour
+{
+    EnemyType enemyTypeToSpawn;
+    Transform spawnPos;
+    //12.40
+    private void FixedUpdate()
+    {
+        ObjectPooler.Instance.SpawnFromPool(enemyTypeToSpawn, spawnPos.position, spawnPos.rotation);
+    }
+}
