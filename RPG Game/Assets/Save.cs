97872c71a6dfa3c22dd6aacd59acc4893a306886
Heba\user <hh440555@gmail.com>
2019-02-23 10:53:59 +0200

    save $ load position for player

---
+++ RPG Game/Assets/Save.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour{
+
+	public void SavePosition()
+    {
+        PlayerPrefs.SetFloat("PlayerX", transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", transform.position.y);
+        PlayerPrefs.SetFloat("PlayerZ", transform.position.z);
+
+    }
+    public void LoadPosition()
+    {
+        float x = PlayerPrefs.GetFloat("PlayerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+        float z = PlayerPrefs.GetFloat("PlayerZ");
+
+        transform.position = new Vector3 (x, y, z);
+    }
+}
