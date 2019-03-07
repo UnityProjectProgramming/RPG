9478c7efce8896f5951e5bc4cfcde743e7504f44
Heba\user <hh440555@gmail.com>
2019-03-07 20:14:42 +0200

    fade to main menu && fade between main menu and level1(village)

---
+++ RPG Game/Assets/LevelChanger.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+
+    private int levelToLoad;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToNextLevel();
+        }
+	}
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
+
