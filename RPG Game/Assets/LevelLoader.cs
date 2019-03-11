303b0e408ae99c89726e091bc5183a4734a1d49f
Heba\user <hh440555@gmail.com>
2019-03-11 05:08:40 +0200

    LOADING BAR  for level 1

---
+++ RPG Game/Assets/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+    public GameObject LoadingScreen;
+    public Slider slider;
+    public Text progressText;
+	public void LoadLevel( int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+}
