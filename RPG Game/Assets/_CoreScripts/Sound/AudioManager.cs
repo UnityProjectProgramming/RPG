80abdd25cda43f09e0d243f6d154ffe02fed710d
MazenMorgan <morgan.mazen42@gmail.com>
2019-03-11 23:17:22 +0200

    Creating AudioManager and making main theme sound persistant through levels

---
+++ RPG Game/Assets/_CoreScripts/Sound/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+
+    [SerializeField] Sound[] sounds;
+
+    public static AudioManager instance;
+	// Use this for initialization
+	void Awake ()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+		foreach(Sound s in sounds)
+        {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.clip = s.clip;
+            s.audioSource.volume = s.volume;
+            s.audioSource.pitch = s.pitch;
+            s.audioSource.loop = s.loop;
+        }
+	}
+
+    private void Start()
+    {
+        Play("MainTheme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + s.name + " is Invalid!");
+            return;
+        }
+        s.audioSource.Play();
+        // Put this anywhere FindObjOfType<AudioManager>().Play("SOUND NAME");
+    }
+}
---
+++ RPG Game/Assets/_CoreScripts/Sound/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0.0f, 1.0f)]
+    public float volume;
+    [Range(0.0f, 1.0f)]
+    public float pitch;
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+	
+}
