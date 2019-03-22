b6db113cf45473c87880dd319bd7b02df3244283
Heba\user <hh440555@gmail.com>
2019-03-22 07:50:47 +0200

    attempt to connect menu

--- RPG Game/Assets/_Levels/MainMenu/MainMenu.cs
+++ RPG Game/Assets/_Levels/MainMenu/MainMenu.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
     public void QuitGame()
     {
         Debug.Log("Quit!");
