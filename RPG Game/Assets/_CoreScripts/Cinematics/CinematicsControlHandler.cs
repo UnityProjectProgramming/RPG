dcb1d614f72c6489e3032df0e262f4661fc31ba4
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-24 16:49:05 +0200

    Subscripting to the PlayableDirector action Play, Stopped to disable control and re-enabling it after cinematic's period is done

---
+++ RPG Game/Assets/_CoreScripts/Cinematics/CinematicsControlHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlHandler : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            // Using Action Event "Delegates" ... The observer pattern
+            PlayableDirector playableDirector = GetComponent<PlayableDirector>();
+            playableDirector.played += DisableControl;
+            playableDirector.stopped += EnableControl;
+        }
+
+        void DisableControl(PlayableDirector playableDirector)
+        {
+            print("DisableControl");
+        }
+
+        void EnableControl(PlayableDirector playableDirector)
+        {
+            print("EnableControl");
+        }
+    }
+
+}
