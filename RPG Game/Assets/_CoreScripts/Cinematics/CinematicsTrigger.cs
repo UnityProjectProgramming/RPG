973a9f186717fe52d328ab86a5408a92316dc93a
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-24 16:04:15 +0200

    Creating CinematicsTrigger script

---
+++ RPG Game/Assets/_CoreScripts/Cinematics/CinematicsTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsTrigger : MonoBehaviour
+    {
+        // Private
+        private bool hasTriggerd = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(!hasTriggerd && other.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                hasTriggerd = true;
+            }
+        }
+    }
+}
+
+
