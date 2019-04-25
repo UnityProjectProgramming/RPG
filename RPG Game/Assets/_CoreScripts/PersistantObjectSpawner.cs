aa20e6a4b519220b53cc0ab3f78a83a2b55baaa8
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-26 00:08:17 +0200

    Optimizing Scene management and adjusting prefabs, laying the ground for persistant object spawner to spawn

---
+++ RPG Game/Assets/_CoreScripts/PersistantObjectSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistantObjectSpawner : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
