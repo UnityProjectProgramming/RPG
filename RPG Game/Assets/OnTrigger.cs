13bb983fafbc156a975db1815e235079af5d7c62
Heba\user <hh440555@gmail.com>
2019-04-25 22:10:02 +0200

    the connect code

---
+++ RPG Game/Assets/OnTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTrigger: MonoBehaviour
+{
+    public string nextLevel;
+    
+    public GameObject Text;
+    
+
+    void Start()
+    {
+        Text.SetActive(false);
+    }
+    void OnTriggerStay(Collider otherObjects)
+    {
+        if (otherObjects.gameObject.tag == "Player")
+        {
+            Text.SetActive(true);
+            if (Input.GetButtonDown("Press"))
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+        }
+    }
+    void OnTriggerExit(Collider otherObjects)
+    {
+        if (otherObjects.gameObject.tag == "Player")
+        {
+            Text.SetActive(false);
+        }
+    }
+}
