e92bc0c64bc8d25f286bd4c06978d9a19aa89197
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-25 14:38:45 +0200

    Fixed Camera Priority

---
+++ RPG Game/Assets/VirtiualCameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class VirtiualCameraController : MonoBehaviour {
+
+    [SerializeField] Cinemachine.CinemachineVirtualCamera PlayerVirtualCamera;
+
+    private Cinemachine.CinemachineBrain CinemachineBrain;
+
+    // Use this for initialization
+    void Start ()
+    {
+        CinemachineBrain = GetComponent<Cinemachine.CinemachineBrain>();
+        PlayableDirector playableDirector = GetComponent<PlayableDirector>();
+
+    }
+
+    void SetOriginalPlayerVirtualCamera(PlayableDirector pd)
+    {
+       
+    }
+
+}
