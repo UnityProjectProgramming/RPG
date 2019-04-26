a74427fa0d8edbb190961a1e4ebb24a0fc21bf94
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-27 01:14:33 +0200

    Creating Basic infrastructure for saving system

---
+++ RPG Game/Assets/_CoreScripts/Saving/SavingSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Saving
+{
+    public class SavingSystem : MonoBehaviour
+    {
+        public void Save(string saveFile)
+        {
+            Debug.Log("Saving To " + saveFile);
+        }
+
+        public void Load(string saveFile)
+        {
+            Debug.Log("Loading From " + saveFile);
+        }
+    }
+}
+
---
+++ RPG Game/Assets/_CoreScripts/Saving/SavingWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Saving
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+
+        const string DEFAULT_SAVE_FILE = "save";
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                GetComponent<SavingSystem>().Save(DEFAULT_SAVE_FILE);
+            }
+
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                GetComponent<SavingSystem>().Load(DEFAULT_SAVE_FILE);
+            }
+        }
+    }
+
+}
