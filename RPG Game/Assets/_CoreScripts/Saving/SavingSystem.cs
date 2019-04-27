eec67ba8363ee40225ac188cfae92bde5885ae07
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-27 02:22:38 +0200

    Testing writing in file as bytes and using Unicode UTF8

--- RPG Game/Assets/_CoreScripts/Saving/SavingSystem.cs
+++ RPG Game/Assets/_CoreScripts/Saving/SavingSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.IO;
+using System.Text;
 
 namespace RPG.Saving
 {
@@ -7,7 +8,12 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            Debug.Log("Saving To " + GetPathFromSavingFile(saveFile));
+            string path = GetPathFromSavingFile(saveFile);
+            Debug.Log("Saving To " + path);
+            FileStream stream = File.Open(path, FileMode.Create);
+            byte[] bytes = Encoding.UTF8.GetBytes("Kore De Owari Da !!");
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Close(); // main reason why we close file is because when we File.Open it sends a msg to the OS to give us a file handle and we must free the file handle incase we run out of resuource
         }
 
         public void Load(string saveFile)
@@ -17,10 +23,8 @@ namespace RPG.Saving
 
         private string GetPathFromSavingFile(string saveFile)
         {
-
             saveFile += ".sav";
             return Path.Combine(Application.persistentDataPath, saveFile);
-
         }
     }
 }
