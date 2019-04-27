c79eeb4365f9a871c367c059d85f354aadab02ff
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-27 16:18:29 +0200

    Serializing and Deserializing Player Position

--- RPG Game/Assets/_CoreScripts/Saving/SavingSystem.cs
+++ RPG Game/Assets/_CoreScripts/Saving/SavingSystem.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.IO;
 using System.Text;
+using System;
 
 namespace RPG.Saving
 {
@@ -12,8 +13,9 @@ namespace RPG.Saving
             Debug.Log("Saving To " + path);
             using (FileStream stream = File.Open(path, FileMode.Create)) // it will close the file on its own.
             {
-                byte[] bytes = Encoding.UTF8.GetBytes("Kore De Owari Da !!");
-                stream.Write(bytes, 0, bytes.Length);
+                Transform playerTransform = GetPlayerTransform();
+                byte[] buffer = SerializeVector(playerTransform.position);
+                stream.Write(buffer, 0, buffer.Length);
             }
 
         }
@@ -26,9 +28,37 @@ namespace RPG.Saving
             {
                 byte[] buffer = new byte[stream.Length];
                 stream.Read(buffer, 0, buffer.Length);
-                string saveData = Encoding.UTF8.GetString(buffer);
-                Debug.Log("Save Data: " + saveData);
+                Vector3 playerPos = DeseralizeVector(buffer);
+                SetPlayerTransform(playerPos);
             }
+        }
+
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte[] vectorBytes = new byte[3 * sizeof(float)];
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
+            return vectorBytes;
+        }
+
+        private Vector3 DeseralizeVector(byte[] buffer)
+        {
+            Vector3 result = new Vector3();
+            result.x = BitConverter.ToSingle(buffer, 0);
+            result.y = BitConverter.ToSingle(buffer, 4);
+            result.z = BitConverter.ToSingle(buffer, 8);
+            return result;
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
+        private void SetPlayerTransform(Vector3 newPos)
+        {
+            GameObject.FindWithTag("Player").transform.position = newPos;
         }
 
         private string GetPathFromSavingFile(string saveFile)
