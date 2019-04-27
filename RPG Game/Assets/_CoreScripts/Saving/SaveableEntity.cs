d1eb76c9c96830af1c3ce5a6a87b8c2bd6ab5b13
MazenMorgan <morgan.mazen42@gmail.com>
2019-04-28 01:05:43 +0200

    Laying the foundation for GUID(UUID) to be generated

--- RPG Game/Assets/_CoreScripts/Saving/SaveableEntity.cs
+++ RPG Game/Assets/_CoreScripts/Saving/SaveableEntity.cs
@@ -2,11 +2,14 @@
 
 namespace RPG.Saving
 {
+    [ExecuteInEditMode]
     public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField] string uniqueIdentifier = "";
+
         public string GetUniqueIdentifier()
         {
-            return "";
+            return uniqueIdentifier;
         }
 
         public object CaptureState()
@@ -18,6 +21,14 @@ namespace RPG.Saving
         public void RestoreState(object state)
         {
             print("Restoring State for " + GetUniqueIdentifier());
+        }
+
+        private void Update()
+        {
+            if(!Application.isPlaying)
+            {
+                Debug.Log("Editing");
+            }
         }
     }
 }
