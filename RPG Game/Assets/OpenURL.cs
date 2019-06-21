3d92802fff4d9b5c4f8f1ef3628f26d5c0829062
MazenMorgan <morgan.mazen42@gmail.com>
2019-06-22 01:16:07 +0200

    Optimizing and creating feedback scene

---
+++ RPG Game/Assets/OpenURL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+
+    public void GoToURL()
+    {
+        System.Diagnostics.Process.Start("https://forms.gle/VKnwbHGMMh9mRqPf7");
+    }
+
+
+}
